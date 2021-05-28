b4f1a1c53927d986316e34b84b49ce36f4667c40
mwangemi <mwangemik@gmail.com>
2021-05-28 22:54:05 +0300

    hide user credentials to environ

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,18 +138,9 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 
-EMAIL_HOST_USER = 'mwangemik@gmail.com'
-
-print('email for the user', EMAIL_HOST_USER)
-
-
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-
-EMAIL_HOST_PASSWORD = 'Malaikaa@2030'
-
-print('password for the user', EMAIL_HOST_PASSWORD)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
